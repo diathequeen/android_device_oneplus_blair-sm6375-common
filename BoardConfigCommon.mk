42abd1fcdb0ffc64595d92066c36eff81202199b
DiamondQueen <ghostieqwe@gmail.com>
2026-09-19 22:25:06 +0300

    sm6375_blair-common: more referencing from sm8850-common

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -98,6 +98,22 @@ endif
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
 
+# Kernel
+BOARD_BOOTCONFIG := \
+    androidboot.hardware=qcom \
+    androidboot.hypervisor.protected_vm.supported=true \
+    androidboot.hypervisor.version=gunyah \
+    androidboot.load_modules_parallel=true \
+    androidboot.memcg=1 \
+    androidboot.serialconsole=0 \
+    androidboot.usbcontroller=a600000.dwc3 \
+    androidboot.vendor.qspa=true
+
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_USES_GENERIC_KERNEL_IMAGE := true
+
 # Partitions
 BOARD_PRODUCTIMAGE_MINIMAL_PARTITION_RESERVED_SIZE := false
 -include vendor/lineage/config/BoardConfigReservedSize.mk
